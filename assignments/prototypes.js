b70c1876e561d7d5b0f08486c413eba31bb95a73
john2796 <benedictmiranda27@yahoo.com>
2018-11-21 22:17:29 -0800

    working on stretch

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -7,13 +7,22 @@
   
   Each constructor function has unique properites and methods that are defined in their block comments below:
 */
-  
+
 /*
   === GameObject ===
   * createdAt
   * dimensions
-  * destroy() // prototype method -> returns the string: 'Object was removed from the game.'
+  * destroy() // prototype method -> returns the string: '<object > was removed from the game.'
 */
+function GameObject(gameObjectAttrs) {
+  this.createdAt = gameObjectAttrs.createdAt;
+  this.dimensions = gameObjectAttrs.dimensions;
+}
+// Prototype destroy 
+GameObject.prototype.destroy = function () {
+  return `${this.name} was removed from the game`
+}
+
 
 /*
   === CharacterStats ===
@@ -22,6 +31,16 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(charAttrs) {
+  GameObject.call(this, charAttrs);
+  this.hp = charAttrs.hp;
+  this.name = charAttrs.name;
+}
+// Link child prototype building block to parent building block
+CharacterStats.prototype = Object.create(GameObject.prototype);
+CharacterStats.prototype.takeDamage = function () {
+  return `${this.name} took damage`;
+}
 
 /*
   === Humanoid ===
@@ -32,7 +51,19 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(humanoidAttrs) {
+  // inherit attributes
+  CharacterStats.call(this, humanoidAttrs);
+  this.faction = humanoidAttrs.faction;
+  this.weapons = humanoidAttrs.weapons;
+  this.language = humanoidAttrs.language;
+}
+// inherit prototypes
+Humanoid.prototype = Object.create(CharacterStats.prototype);
+Humanoid.prototype.greet = function () {
+  return `${this.name} offers a greeting in ${this.language}.`
+}
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,70 +72,153 @@
 
 // Test you work by uncommenting these 3 objects and the list of console logs below:
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    hp: 5,
-    name: 'Bruce',
-    faction: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Toungue',
-  });
 
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    hp: 15,
-    name: 'Sir Mustachio',
-    faction: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Toungue',
-  });
+const mage = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  hp: 5,
+  name: 'Bruce',
+  faction: 'Mage Guild',
+  weapons: [
+    'Staff of Shamalama',
+  ],
+  language: 'Common Toungue',
+});
 
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    hp: 10,
-    name: 'Lilith',
-    faction: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
+const swordsman = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 2,
+    height: 2,
+  },
+  hp: 15,
+  name: 'Sir Mustachio',
+  faction: 'The Round Table',
+  weapons: [
+    'Giant Sword',
+    'Shield',
+  ],
+  language: 'Common Toungue',
+});
 
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.hp); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.faction); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+const archer = new Humanoid({
+  createdAt: new Date(),
+  dimensions: {
+    length: 1,
+    width: 2,
+    height: 4,
+  },
+  hp: 10,
+  name: 'Lilith',
+  faction: 'Forest Kingdom',
+  weapons: [
+    'Bow',
+    'Dagger',
+  ],
+  language: 'Elvish',
+});
 
-  // Stretch task: 
-  // * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.hp); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.faction); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// Stretch task: 
+// * Create Villian and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villian and one a hero and fight it out with methods!
+
+function JohnHero(mikkoAttributes) {
+  Humanoid.call(this, mikkoAttributes);
+  this.village = mikkoAttributes.village;
+}
+JohnHero.prototype = Object.create(Humanoid.prototype);
+JohnHero.prototype.fire = function () {
+  return `fire 🔥damage -5hp your health is now ${this.hp - 5}hp`
+}
+JohnHero.prototype.water = function () {
+  return `Water 💧 damage -10hp your health is now ${this.hp - 10}hp`
+}
+JohnHero.prototype.earth = function () {
+  return `Earth 🌴 damage -15hp your health is now ${this.hp - 15}hp`
+}
+JohnHero.prototype.air = function () {
+  return `Air 💨 damage -25hp your health is now ${this.hp - 25}hp`
+}
+JohnHero.prototype.combo = function () {
+  return `COMBOOOOO !!! 😎😹 Air 💨,Earth 🌴,Water 💧,fire 🔥 damage -100hp your health is now ${this.hp - 100}hp`
+}
+
+// Stretch Villain
+function VillainHero(villainAttributes) {
+  Humanoid.call(this, villainAttributes);
+  this.village = villainAttributes.village;
+}
+VillainHero.prototype = Object.create(Humanoid.prototype);
+VillainHero.prototype.fart = function () {
+  this.johnHealth -= 10;
+  return `Fart 💩-10hp damage ${this.johnHealth}hp`
+}
+VillainHero.prototype.poison = function () {
+  return `Poison 🐍 -25hp damage ${this.hp - 25}hp`
+}
+
+// stretch char mikkoAvater
+const john = new JohnHero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  johnHealth: 100,
+  name: 'John Ꭶ Hokage',
+  faction: 'Avatar Guild',
+  weapons: [
+    'Fire',
+    'Water',
+    'Earth',
+    'Air'
+  ],
+  language: 'Avaterian',
+  village: 'Konoha Village'
+});
+
+const villain = new VillainHero({
+  createdAt: new Date(),
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  villainHealth: 100,
+  name: 'Ikimaru ☯︎',
+  faction: 'Villain Guild',
+  weapons: [
+    'fart'
+  ],
+  language: 'Villany',
+  village: 'Hidden Village'
+});
+
+
+
+// Story
+console.log(`The battle of ${john.name}  of ${john.village} and ${villain.name} of ${villain.village} the most dangerous enemy of ${john.village} `);
+console.log(`One day the peaceful ${john.village} was attacked by ${villain.name} and killed 100 people of ${john.village}`)
+console.log(`${john.name} heard the bad news and immedietely look for ${villain.name} and challenge him to duel him to leave his village so no one else get hurt and they`);
+console.log(`The next morning ${john.name} ${villain.name} began their duel`);
+console.log(`First the ${villain.name} attacked ${john.name} with his fart 😹 power ${villain.fart()} ${john.johnHealth}`)
+
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,59 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window/Global = When in the global scope, the value of this will be the window/console Object.
+* 2. Implicit Binding = whenever a function is called by a preceding dot, the object before the dot is this
+* 3. New Binding = Whenever a constructor function is used , this refers to the specific instance of the object that is created and returned by the constructor function 
+* 4. Explicit binding = Whenever Javascript's call or apply method is used, this is explicitly defined.
 *
+
+
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+function sayAge(age) {
+  console.log(this);
+  return age;
+}
+sayAge('Monkey D Luffy');
 // code example for Window Binding
 
 // Principle 2
+const intern = {
+  firstName: 'John',
+  lastName: 'Miranda',
+  greeting: 'Hello',
+  sayHello: function (age) {
+    console.log(`${this.greeting} my name is ${this.firstName} ${this.lastName} and i am ${age} years old!`)
+  }
+}
+intern.sayHello(23);
 
 // code example for Implicit Binding
 
 // Principle 3
-
+function Intern(name) {
+  this.greeting = 'Hello',
+    this.name = name,
+    this.speak = function () {
+      console.log(`${this.greeting} my name is ${name}`);
+    }
+}
+const janine = new Intern('Janine');
+const mikko = new Intern('Mikko');
+console.log(janine.speak())
+console.log(mikko.speak())
 // code example for New Binding
 
 // Principle 4
-
+function Intern(name) {
+  this.greeting = 'Hello',
+    this.name = name,
+    this.speak = function () {
+      console.log(`${this.greeting} my name is ${name}`);
+    }
+}
+const janine = new Intern('Janine');
+janine.speak.apply(janine);
 // code example for Explicit Binding
